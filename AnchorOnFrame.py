fa3727af65cd7adf873bc7bd729282cd77d73c41
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-06-18 18:23:04 +0530

    Part 5

---
+++ AnchorOnFrame.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root=Tk()
+root.title("@happy_coding01")
+root.geometry("300x300")
+frame1=Frame(root,bg="blue",height=100,width=100)
+frame1.pack(anchor="nw")
+#(anchor="nw","ne","sw","se")
+root.mainloop()
