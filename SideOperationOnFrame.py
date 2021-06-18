3114fb78bd8c6eb7351ce0d233182724c7d0a055
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-06-18 18:22:39 +0530

    Part 4

---
+++ SideOperationOnFrame.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root=Tk()
+root.title("@happy_coding01")
+root.geometry("300x300")
+frame1=Frame(root,bg="blue",height=100,width=100)
+frame1.pack(side=RIGHT)
+#(side=TOP,BOTTOM,LEFT,RIGHT)
+root.mainloop()
