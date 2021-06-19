47bb45bb9dfc50dab4b7d07e11730eefd6bfbce0
Hitansh Kadakia <hitanshchiragkadakia@gmail.com>
2021-06-19 14:00:06 +0530

    Part 8

---
+++ CreateALabel.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root=Tk()
+root.title("@happy_coding01")
+root.geometry("300x300")
+label=Label(root,text="Hello World",fg="red",font=("arial",15,"bold"))
+label.pack()
+root.mainloop()
